52aa2408dcd82d71d426054a9443b6c7b08099dd
TkachevAV <andreytkach233@gmail.com>
2024-09-26 14:43:50 +1000

    Создание структур и инициализация объектов

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 using namespace std;
-struct Node{
+struct Node{ //Структура для создания узла списка
     int data;
-    Node* pred;
+    Node* prev;
     Node* next;
+};
+
+struct DoubleLinkedlist{ //Структура для создания всего списка
+    Node* head;
+    Node* tail;};
+
+void initializeNode(Node* node, int value) { //Процедура инициализации узла
+    node->data = value;
+    node->prev = nullptr;
+    node->next = nullptr;
 }
 
 
-int main(){
 
+void initializeList(DoubleLinkedlist* node) { //Процедура иниициализации списка
+    node->head = nullptr;
+    node->tail = nullptr;
+}
+
+
+
+
+void print_list (DoubleLinkedlist* temp){ //Процедура ввывода элементов списка
+    Node* current = temp -> head;
+    while (current){
+        cout<<current->data<<endl;
+        current=current->next;
+    }
+
+}
+
+int main(){
+    cout<<"Компилируется файл..."<<endl;
 
     return 0;
 }
